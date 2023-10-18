6dedef883f17cd651d76d3c8fbf1d48bc2db73f0
rrymar <rrymar@outlook.com>
2023-10-18 19:22:59 +0300

    Init

---
+++ AwaitTask/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Before async"); 
+        Console.WriteLine("Initial Thread Id:" + Thread.CurrentThread.ManagedThreadId);
+        var fooAsync = FooAsync();
+        Console.WriteLine("After async"); 
+        var fooAsyncWithWait = FooAsyncWithAwait(); 
+        Console.WriteLine("Before await"); 
+        await fooAsync;
+        await fooAsyncWithWait;
+        Console.WriteLine("After await"); 
+        Console.WriteLine("DONE");
+    }
+
+    static async Task FooAsync()
+    {
+        Task.Delay(1000).Wait();
+        Console.WriteLine("Async method that doesn't have await");  
+        Console.WriteLine("Thread Id:" + Thread.CurrentThread.ManagedThreadId);
+    }
+
+    static async Task FooAsyncWithAwait()
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("Async method that have await"); 
+        Console.WriteLine("Thread Id:" + Thread.CurrentThread.ManagedThreadId);
+    }
+
+}
